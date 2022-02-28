b3bc349f9fabe56ce342fdc7d74c02303aba97ff
Kyleah Justice <kikij432@gmail.com>
2022-02-28 04:08:03 -0700

    to add and update an employee and employee role

--- server.js
+++ server.js
@@ -101,3 +101,55 @@ app.get('/api/employee', (req, res) => {
 });
 
 
+app.post('/api/employee', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'role_id', 'manager_name');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_name)
+                    VALUES (?, ?, ?, ?)`;
+    const params = [body.first_name, body.last_name, body.role_id, body.manager_name];
+    
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+  });
+
+  app.put('/api/employee/:id', (req, res) => {
+    const sql = `UPDATE employee SET role_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.role_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        // check if a record was found
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Employee not found!'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).end();
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
