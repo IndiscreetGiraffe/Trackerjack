7dd017779c4133f499beb64cac4b10cabed1eeb1
Kyleah Justice <kikij432@gmail.com>
2022-02-28 03:51:52 -0700

    putting the server.js together first

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const { express } = require('express');
+const express = require('express');
+const inputCheck = require('./utils/inputCheck');
 const db = require('./db/connection');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
 
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.get('/api/department', (req, res) => {
+    const sql = `SELECT * FROM department`;
+
+    db.query(sql, (err, row) => {
+        if (err) {
+            res.status(500).json({ error: "Error"});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
